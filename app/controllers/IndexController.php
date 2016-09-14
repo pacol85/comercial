ff1e0a7ea06e271fcba85d3395617338fad61296
Paco <newreality64@gmail.com>
2016-09-14 01:21:02 -0600

    Se corrigió la manera de funcionar de la pantalla de inicio haciendo las validaciones correspondientes y obligando el cambio de la contraseña inicial.

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -15,10 +15,10 @@ class IndexController extends ControllerBase
     
     public function entrarAction(){
     		//$this->view->disable();
-		$user = $this->request->get("user");
+		$user = parent::gPost("user");
 		$user = trim($user);
 		$user = strtoupper($user);
-		$pass = $this->request->get("pass");
+		$pass = parent::gPost("pass");
 		$pass = trim($pass);
 		// Ver si existe el usuario y contraseña
 		$success = Usuario::find("usuario ='$user'");
@@ -29,13 +29,11 @@ class IndexController extends ControllerBase
 	
 			//validar contrasena
 			if(parent::checkPass($pass, $usuario->clave)){
-				$this->session->set("usuario", $usuario->id);
-
-				
-				if(parent::checkPass($usuario->clave, "",true)){
+				$this->session->set("usuario", $usuario->id);				
+				if(parent::checkPass($pass, "",true)){
 					parent::forward("index", "newPass");					
 				}else{
-					parent::forward("index", "index");					
+					parent::forward("inicio", "index");					
 				}
 			}else{
 				parent::msg("Credenciales suministradas son err&oacute;neas");
@@ -43,7 +41,7 @@ class IndexController extends ControllerBase
 			}
 			 
 		} else {
-			parent::msg("no existe el usuario");
+			parent::msg("Usuario no encontrado");
 			parent::forward("index", "retry");
 		}
 	}
@@ -53,51 +51,51 @@ class IndexController extends ControllerBase
 	}
 	
     public function changePassAction(){
-    	$usuario = $this->session->get("usuario");
-    	$op = $this->request->getPost("oldPass");
-    	$np = $this->request->getPost("newPass");
-    	$rp = $this->request->getPost("repeatPass");
+    	$usuario = parent::gSession("usuario");
+    	$op = parent::gPost("oldPass");
+    	$np = parent::gPost("newPass");
+    	$rp = parent::gPost("repeatPass");
     	$exito = "";
     	$error = "";
     		
     	if(($op == null || $op == "") and ($np == null || $np == "") and ($rp == null || $rp == "")){
-    		$this->flashSession->error("Alguno de los campos solicitados no fue llenado");
-    		return $this->dispatcher->forward(array(
-    				"controller" => "inicio",
-    				"action" => "newPass"
-    		));
+    		parent::msg("Alguno de los campos solicitados no fue llenado");
+    		parent::forward("index", "newPass");
     			
     	}else{
-    		$u = Usuarios::findFirst($usuario);
-    		if(!$this->security->checkHash($op, $u->u_contrasena)){
-    			$error = "Contrase&ntilde;a incorrecta";
+    		$u = Usuario::findFirst($usuario);
+    		if(!$this->security->checkHash($op, $u->clave)){
+    			$error = "Contrase&ntilde;a original incorrecta";
     		}
     		if($np != $rp){
     			$error = "Las contrase&ntilde;as no concuerdan";
     		}
+    		$oldClaves = ClavesUsuario::find("usuario = $u->id");
+    		foreach ($oldClaves as $oc){
+    			if(parent::checkPass($np, $oc->clave)){
+    				$error = "No se pueden utilizar las &uacute;ltimas 5 claves";
+    			}
+    		}
+    		
     		if(preg_match('/^(?=.{8,}$)(?=.*?[A-Z])(?=.*?([\x20-\x40\x5b-\x60\x7b-\x7e\x80-\xbf]).*?$).*$/',$np)){
     			$exito = "Contrase&ntilde;a cambiada exitosamente";
     		}else{
     			$error = "Nueva contrase&ntilde;a no cumple con los estandares";
     		}
     		if($error != ""){
-    			$this->flashSession->error($error);
-    			return $this->dispatcher->forward(
-    					array(
-    							"controller" => "inicio",
-    							"action"     => "newPass"
-    					)
-    			);
+    			parent::msg($error);
+    			parent::forward("index", "newPass");
     		}else{
-    			$u->u_contrasena = $this->security->hash($np);
-    			$u->save();
-    			$this->flash->success($exito);
-    			return $this->dispatcher->forward(
-    					array(
-    							"controller" => "inicio",
-    							"action"     => "index"
-    					)
-    			);
+    			//guardar clave anterior en listado de claves
+    			$claves = new ClavesUsuario();
+    			$claves->clave = $u->clave;
+    			$claves->usuario = $u->id;
+    			$claves->save();
+    			
+    			$u->clave = $this->security->hash($np);
+    			$u->update();
+    			parent::msg($exito, "s");
+    			parent::forward("inicio", "index");
     		}
     	}
     }
