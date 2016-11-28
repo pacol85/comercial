1ad3611b3951f5892b5bbfd089e438ebbaed474a
Paco <newreality64@gmail.com>
2016-11-27 23:28:16 -0600

    Basic Client functionality, missing details on edition and missing disabling

--- app/controllers/ClienteController.php
+++ app/controllers/ClienteController.php
@@ -13,7 +13,7 @@ class ClienteController extends ControllerBase
 		
 		$campos = [
 				["t", ["nombre"], "Nombre Completo"],
-				["sel", ["tipoDoc", ["d" => "DUI", "n" => "NIT"]], "Tipo Documento"],
+				["sel", ["tipoDoc", ["1" => "DUI", "2" => "NIT"]], "Tipo Documento"],
 				["t", ["doc"], "Documento"],
 				["d", ["expedicion"], "Fecha Expedici&oacute;n"],
 				["t", ["lugar"], "Lugar Expedici&oacute;n"],
@@ -26,6 +26,7 @@ class ClienteController extends ControllerBase
 				["t", ["trabajo"], "Trabajo"],
 				["t", ["area"], "Area de trabajo"],
 				["t", ["cargo"], "Cargo"],
+				["t", ["jefe"], "Jefe"],
 				["d", ["fdesde"], "Desde"],
 				["m", ["sueldo", 0], "Sueldo"],
 				["t", ["tofic"], "Tel&eacute;fono Oficina"],
@@ -40,7 +41,6 @@ class ClienteController extends ControllerBase
 		$tabla = parent::thead("clientes", $head);
 		$clientes = Cliente::find();
 		foreach ($clientes as $c){
-			$s = Sucursal::findFirst("id = ".$e->sucursal);
 			$deshabilitar = "Deshabilitar";
 			if($c->estado == 0){
 				$deshabilitar = "Habilitar";
@@ -49,11 +49,11 @@ class ClienteController extends ControllerBase
 			if($c->alquila == 0){
 				$alquila = "No";
 			}
-			$m = Municipios::findFirst("municipio = $c->municipio");
+			$m = Municipios::findFirst("id = $c->municipio");
 			$tabla = $tabla.parent::tbody([
 					$c->nombre,
 					$c->documento,
-					$m,
+					$m->nombre,
 					$alquila,
 					$c->trabajo,
 					$c->sueldo,
@@ -61,7 +61,7 @@ class ClienteController extends ControllerBase
 					parent::a(2, "cargarDatos('".$c->id."', '".$c->nombre."', '".$c->tipodoc."', '".
 							$c->documento."', '".$c->fexpedicion."', '".$c->lugarExpedicion."', '".$c->direccion."', '".
 							$m->departamento."', '".$m->id."', '".$c->alquila."', '".$c->propietario."', '".
-							$c->trabajo."', '".$c->areaTrab."', '".$c->cargo."', '".$c->fdesde."', '".
+							$c->trabajo."', '".$c->areaTrab."', '".$c->cargo."', '".$c->jefe."', '".$c->fdesde."', '".
 							$c->sueldo."', '".$c->telOficina."')", "Editar")." | ".
 					parent::a(1, "cliente/deshabilitar", $deshabilitar, [["id", $c->id]])
 			]);
@@ -69,7 +69,7 @@ class ClienteController extends ControllerBase
 		
 		//js
 		$fields = ["id", "nombre", "tipoDoc", "doc", "expedicion", "lugar", "dir", "dept", "muni", "alquila", 
-				"propietario", "trabajo", "area", "cargo", "fdesde", "sueldo", "tofic"];
+				"propietario", "trabajo", "area", "cargo", "jefe", "fdesde", "sueldo", "tofic"];
 		$otros = "";
 		$jsBotones = ["form1", "cliente/edit", "cliente/index"];
 		
@@ -77,108 +77,100 @@ class ClienteController extends ControllerBase
 	}
 	
 	public function guardarAction(){
-		if(parent::vPost("pNombre") && parent::vPost("pApellido") && parent::vPost("dir") && parent::vPost("fIngreso") &&
-				parent::vPost("salario") && parent::vPost("dui") && parent::vPost("nit") ){
-			$dui = parent::gPost("dui");
-			$nit = parent::gPost("nit");
-			$isss = parent::gPost("isss");
-			$afp = parent::gPost("afp");
-			$emp = Empleado::find("dui like '$dui' or nit like '$nit' or isss like '$isss' or afpnum like '$afp'");
-			if(count($emp) > 0){
-				parent::msg("Los documentos ingresados equivalen a los de otro usuario ya existente");
-				return parent::forward("empleado", "index");
-			}//else{
-				//crear nuevo empleado
-				$e = new Empleado();
-				$e->primer_nombre = parent::gPost("pNombre");
-				$e->segundo_nombre = parent::gPost("sNombre");
-				$e->primer_apellido = parent::gPost("pApellido");
-				$e->segundo_apellido = parent::gPost("sApellido");
-				$e->apellido_casada = parent::gPost("aCasada");
-				$e->genero = parent::gPost("genero");
-				$e->fecha_nacimiento = parent::gPost("fNac");
-				$e->direccion = parent::gPost("dir");
-				$e->fecha_ingreso = parent::gPost("fIngreso");
-				$e->sucursal = parent::gPost("suc");
-				$e->salario = parent::gPost("salario");
-				$e->dui = parent::gPost("dui");
-				$e->nit = parent::gPost("nit");
-				$e->isss = parent::gPost("isss");
-				$e->afp = parent::gPost("afp");
-				$e->afpnum = parent::gPost("nafp");
-				$e->fcreacion = parent::fechaHoy(true);
-				$e->fmod = parent::fechaHoy(true);
-				$e->estado = 1;
-				if($e->save()){
-					parent::msg("El empleado fue creado exitosamente", "s");
-				}else{
-					parent::msg("Ocurri&oacute; un error durante la transacci&oacute;n");
-				}
-			//}			
+		if(parent::vPost("nombre") && parent::vPost("doc") && parent::vPost("dir") && parent::vPost("trabajo") &&
+				parent::vPost("fdesde") && parent::vPost("sueldo")){
+			$doc = parent::gPost("doc");
+			$nombre = parent::gPost("nombre");
+			$cli = Cliente::find("documento like '$doc' or nombre like '$nombre'");
+			if(count($cli) > 0){
+				parent::msg("Los documentos ingresados equivalen a los de otro cliente ya existente");
+				return parent::forward("cliente", "index");
+			}
+			//crear nuevo cliente
+			$c = new Cliente();
+			$c->alquila = parent::gPost("alquila");
+			$c->areaTrab = parent::gPost("area");
+			$c->cargo = parent::gPost("cargo");
+			$c->direccion = parent::gPost("dir");
+			$c->documento = parent::gPost("doc");
+			$c->estado = 1;
+			$c->fdesde = parent::gPost("fdesde");
+			$c->fexpedicion = parent::gPost("expedicion");
+			$c->jefe = parent::gPost("jefe");
+			$c->lugarExpedicion = parent::gPost("lugar");
+			$c->municipio = parent::gPost("muni");
+			$c->trabajo = parent::gPost("trabajo");
+			$c->nombre = parent::gPost("nombre");
+			if($c->alquila == 1){
+				$c->propietario = parent::gPost("propietario");
+			}else{
+				$c->propietario = $c->nombre;
+			}			
+			$c->sueldo = parent::gPost("sueldo");
+			$c->telOficina = parent::gPost("tofic");
+			$c->tipodoc = parent::gPost("tipoDoc");
+			if($c->save()){
+				parent::msg("El cliente fue creado exitosamente", "s");
+			}else{
+				parent::msg("Ocurri&oacute; un error durante la transacci&oacute;n");
+			}
 		}else{
 			parent::msg("Aseg&uacute;rese de llenar todos los campos");
 		}
-		parent::forward("empleado", "index");
+		parent::forward("cliente", "index");
 	}
 	
 	public function editAction(){
 		if(!parent::vPost("id")){
-			parent::msg("Empleado no se carg&oacute; correctamente");
-			parent::forward("empleado", "index");
-		}else{
-			$id = parent::gPost("id");
-			$dui = parent::gPost("dui");
-			$nit = parent::gPost("nit");
-			$isss = parent::gPost("isss");
-			$afp = parent::gPost("nafp");
-			
-			$e = Empleado::findFirst("id = $id");
-			$emps = Empleado::find("(dui like '$dui' or nit like '$nit' or isss like '$isss' or afpnum like '$afp')
-					and id not like $id");
-			if(count($emps) > 0){
-				parent::msg("El empleado $e->primer_nombre $e->primer_apellido ya existe con alguno de estos documentos (DUI, NIT, ISSS o AFP");
-				parent::forward("empleado", "index");
-			}else{
-				$e->primer_nombre = parent::gPost("pNombre");
-				$e->segundo_nombre = parent::gPost("sNombre");
-				$e->primer_apellido = parent::gPost("pApellido");
-				$e->segundo_apellido = parent::gPost("sApellido");
-				$e->apellido_casada = parent::gPost("aCasada");
-				$e->genero = parent::gPost("genero");
-				if(parent::vPost("fNac")){
-					$e->fecha_nacimiento = parent::gPost("fNac");
-				}else{
-					$e->fecha_nacimiento = parent::gPost("fNac2");
-				}
-				$e->direccion = parent::gPost("dir");
-				if(parent::vPost("fIngreso")){
-					$e->fecha_ingreso = parent::gPost("fIngreso");
-				}else{
-					$e->fecha_ingreso = parent::gPost("fIngreso2");
-				}
-				$e->sucursal = parent::gPost("suc");
-				$e->salario = parent::gPost("salario");
-				$e->dui = $dui;
-				$e->nit = $nit;
-				$e->isss = $isss;
-				$e->afp = parent::gPost("afp");
-				$e->afpnum = $afp;
-				$e->fmod = parent::fechaHoy(true);
-				if($e->update()){
-					parent::msg("Edici&oacute;n exitosa", "s");
-					parent::forward("empleado", "index");
-				}else{
-					parent::msg("Ocurri&oacute; un error durante la transacci&oacute;n");
-					parent::forward("empleado", "index");
-				}
-			}
-				
+			parent::msg("Cliente no se carg&oacute; correctamente");
+			return parent::forward("cliente", "index");
 		}
+		$id = parent::gPost("id");
+		$doc = parent::gPost("doc");
+		$nombre = parent::gPost("nombre");
+		
+		$c = Cliente::findFirst("id = $id");
+		$cli = Cliente::find("(documento like '$doc' or nombre like '$nombre')
+				and id not like $id");
+		if(count($cli) > 0){
+			parent::msg("El cliente $c->nombre ya existe");
+			return parent::forward("cliente", "index");
+		}
+		$c->alquila = parent::gPost("alquila");
+		$c->areaTrab = parent::gPost("area");
+		$c->cargo = parent::gPost("cargo");
+		$c->direccion = parent::gPost("dir");
+		$c->documento = parent::gPost("doc");
+		$c->fdesde = parent::gPost("fdesde");
+		$c->fexpedicion = parent::gPost("expedicion");
+		$c->jefe = parent::gPost("jefe");
+		$c->lugarExpedicion = parent::gPost("lugar");
+		$c->municipio = parent::gPost("muni");
+		$c->trabajo = parent::gPost("trabajo");
+		$c->nombre = parent::gPost("nombre");
+		if($c->alquila == 1){
+			$c->propietario = parent::gPost("propietario");
+		}else{
+			$c->propietario = $c->nombre;
+		}			
+		$c->sueldo = parent::gPost("sueldo");
+		$c->telOficina = parent::gPost("tofic");
+		$c->tipodoc = parent::gPost("tipoDoc");
+		if($c->update()){
+			parent::msg("Edici&oacute;n exitosa", "s");
+			return parent::forward("cliente", "index");
+		}else{
+			parent::msg("Ocurri&oacute; un error durante la transacci&oacute;n");
+			return parent::forward("cliente", "index");
+		}
+		
+			
+		
 		
 	}
 	
 	public function deshabilitarAction(){
-		$id = parent::gReq("id");
+		/*$id = parent::gReq("id");
 		if($id == "" || $id == null){
 			parent::msg("Empleado no se carg&oacute; correctamente");
 			parent::forward("empleado", "index");
@@ -200,7 +192,9 @@ class ClienteController extends ControllerBase
 		}else{
 			parent::msg("Ocurri&oacute; un error durante la transacci&oacute;n");
 			parent::forward("empleado", "index");
-		}
+		}*/
+		parent::msg("Esta funci&oacute;n no esta activa en este momento");
+		parent::forward("cliente", "index");
 	}
 	
 	public function listMunicipioAction()
